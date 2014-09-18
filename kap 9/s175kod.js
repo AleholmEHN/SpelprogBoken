11bee984c10495a1852651cba55ae75bb286d8f1
AleholmEHN <elin.henriksson@savsjo.se>
2014-09-18 21:29:26 +0200

    Exempel s175 klart

--- kap 9/s175kod.js
+++ kap 9/s175kod.js
@@ -1,3 +1,18 @@
 
 var canvas1 = new RoboroCanvas("canvas1");
-canvas1.circle(100, 100, 50, "red");
+var canvas2 = new RoboroCanvas("canvas2");
+var canvas3 = new RoboroCanvas("canvas3");
+
+canvas1.rectangle(10, 20, 400, 250, "blue");
+canvas1.circle(20, 50, 150, "yellow");
+canvas1.circle(200, 150, 70, "red");
+canvas1.triangle(100, 20, 180, 100, 60, 80, "green");
+canvas1.ring(400, 200, 100, 10, "gray");
+canvas1.line(100, 350, 750, 100, 10, "pink");
+
+canvas2.fill("orange");
+for (var y = 0 ; y <= canvas3.height; y += 5)
+{
+    canvas3.line(0,0,canvas3.width, y, 1, "black");
+    canvas3.line(canvas3.width, canvas3.height, 0, y, 1, "black");
+}
