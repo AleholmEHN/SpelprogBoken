87f2ac77db7b738940cae3d22ee57abad7e0d844
Elin Henriksson <elin.henriksson@savsjo.se>
2015-01-18 21:27:13 +0100

    till eleverna

--- kap 9/kap9_4_extra.js
+++ kap 9/kap9_4_extra.js
@@ -1,10 +1,16 @@
+// Första ritytan
 var rityta = new RoboroCanvas("minCanvas");
 rityta.updatesPerSecond = 30;
+
+// Andra ritytan
 var rityta2 = new RoboroCanvas("minCanvas2");
 rityta2.updatesPerSecond = 60;
+
+// Bollar för de olika rutorna
 var ball = {x: 0, y:0};
 var ball2 = {x: 0, y:0};
 
+// Update för första rutan
 rityta.update = function()
 {
 
@@ -12,13 +18,18 @@ rityta.update = function()
     rityta.circle(ball.x, ball.y, 25, "green");
     ball.x++;
     ball.y++;
+    // Kontroller om bollen utanför rutan
     if (ball.x > rityta.width)
+    {
         ball.x = 0;
+    }
     if (ball.y > rityta.height)
+    {
         ball.y = 0;
-
+    }
 }
 
+// Update för andra rutan
 rityta2.update = function()
 {
 
@@ -26,9 +37,13 @@ rityta2.update = function()
     rityta2.circle(ball2.x, ball2.y, 25, "blue");
     ball2.x++;
     ball2.y++;
+    // Kontroller om bollen utanför rutan
     if (ball2.x > rityta2.width)
+    {
         ball2.x = 0;
+    }
     if (ball2.y > rityta2.height)
+    {
         ball2.y = 0;
-
+    }
 }
---
+++ kap 9/ovningar/kap9.js
@@ -0,0 +1,23 @@
+var rityta = new RoboroCanvas("minCanvas");
+
+with(rityta)
+{
+// Här ska du skriva företagsnamnet
+}
+
+var back = new RoboroCanvas("myBackground");
+
+
+with (back)
+{
+
+    // Använd for-loopar för att rita streck för att få rutnätet
+
+}
+
+rityta.update = function()
+{
+    // Här ska koden in om något ska röra sig
+
+}
+
